a74d62f4a9a7ba3ac7cb9b2355adf82443dc1b13
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-09-02 13:57:10 -0400

    Add files via upload

--- test/test_production.py
+++ test/test_production.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import sys
+sys.path.append("/home/nick/Production-Schedule/src")
+from production import schedule
 
+
+data = pd.read_csv("/home/nick/Production-Schedule/test/forecast.csv")
+
+plan = schedule(data)
+
+print(plan.demand_satisfaction)
